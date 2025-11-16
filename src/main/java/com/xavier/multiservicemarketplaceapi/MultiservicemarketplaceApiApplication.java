aad3ba85e1eb0c45ed385cddd90c63f1f2cf88be
Xavier Francisco <xavierfrancisco353@gmail.com>
2025-11-16 12:36:10 +0000

    first commit

---
+++ src/main/java/com/xavier/multiservicemarketplaceapi/MultiservicemarketplaceApiApplication.java
@@ -0,0 +1,13 @@
+package com.xavier.multiservicemarketplaceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiservicemarketplaceApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiservicemarketplaceApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/xavier/multiservicemarketplaceapi/MultiservicemarketplaceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xavier.multiservicemarketplaceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MultiservicemarketplaceApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
